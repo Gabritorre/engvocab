e45f2e718cab40c657369c73b39ef94d82fcd6ed
Gabritorre <gabritorredev@gmail.com>
2024-02-23 21:12:34 +0100

    Start creating database model

--- vocapp/models.py
+++ vocapp/models.py
@@ -1,19 +1,36 @@
 from django.db import models
 
 # Create your models here.
-
-class Users(models.Model):
-	pass
-
-class Learn(models.Model):
-	pass
-
-class Expressions(models.Model):
-	pass
-
 class Roles(models.Model):
-	pass
+	role = models.CharField(max_length=30, primary_key=True)
+
 
 class Levels(models.Model):
-	pass
+	level = models.CharField(max_length=10, primary_key=True)
+
+class Expressions(models.Model):
+	id = models.IntegerField(primary_key=True)
+	content = models.CharField(max_length=50)
+	translation_it = models.CharField(max_length=50)
+	note = models.CharField(max_length=300)
+	context = models.CharField(max_length=30)
+	example_en = models.CharField(max_length=150)
+	example_it = models.CharField(max_length=150)
+	is_phrasal_verb = models.BooleanField()
+	is_formal = models.BooleanField()
+	role = models.ForeignKey(Roles, on_delete=models.CASCADE)
+	level = models.ForeignKey(Levels, on_delete=models.CASCADE)
+
+class Users(models.Model):
+	username = models.CharField(max_length=30, primary_key=True)
+	password = models.CharField(max_length=100)
+	last_connection = models.DateTimeField()
+
+
+class Learn(models.Model):
+	user = models.ForeignKey(Users, on_delete=models.CASCADE)
+	expression = models.ForeignKey(Expressions, on_delete=models.CASCADE)
+	confidence = models.IntegerField()
+
+
 
