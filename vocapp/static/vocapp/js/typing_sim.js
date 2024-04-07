bc7c6949e6817870bd1c484104fa440b99180205
Gabritorre <gabritorredev@gmail.com>
2024-04-07 11:10:43 +0200

    Fix desynchronization of letters in typing simulation

--- vocapp/static/vocapp/js/typing_sim.js
+++ vocapp/static/vocapp/js/typing_sim.js
@@ -1,8 +1,8 @@
+timers = []
 function type(string, container_id, speed = 1) {
-
 	let interval = 150;
 	if (speed == 1) {
-		interval = 150;
+		interval = 135;
 	}
 	else if (speed == 2) {
 		interval = 100;
@@ -10,11 +10,15 @@ function type(string, container_id, speed = 1) {
 	else if (speed == 3) {
 		interval = 40;
 	}
-	console.log(interval)
 	let container = document.getElementById(container_id);
 	container.innerHTML = "";
+
+	for(let i = 0; i < timers.length; i++) {
+		clearTimeout(timers[i]);
+	}
+
 	for (let i = 0; i < string.length; i++) {
-		setTimeout(() =>{
+		timers[i] = setTimeout(() =>{
 			container.innerHTML += string[i];
 		}, i * interval);
 	}
@@ -24,17 +28,19 @@ function AddEvents() {
 	var nav_items = document.getElementsByClassName('nav_item');
 
 	for (let i = 0; i < nav_items.length; i++) {
-		nav_items[i].addEventListener('mouseover', run_typing);
+		nav_items[i].addEventListener('mouseover', function() {
+			type(nav_items[i].innerHTML.toLowerCase(), "command", 3);
+		});
 		nav_items[i].addEventListener('mouseout', clear_command);
 	}
 }
 
 function clear_command(e) {
+	for(let i = 0; i < timers.length; i++) {
+		clearTimeout(timers[i]);
+	}
 	let container = document.getElementById("command");
 	container.innerHTML = "";
 }
 
-function run_typing(e) {
-	type(this.innerHTML.toLowerCase(), "command", 3)
-}
 
