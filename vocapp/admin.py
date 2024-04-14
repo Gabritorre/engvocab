e1fa2c82f830cdada5dd1f426a49980fecda1f74
Gabritorre <gabritorredev@gmail.com>
2024-04-14 19:13:19 +0200

    Adjust visualization in admin page

--- vocapp/admin.py
+++ vocapp/admin.py
@@ -8,7 +8,7 @@ class UserAdmin(UserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ["username", "password", "is_staff"]
+    list_display = ["username", "is_staff"]
     list_filter = ["is_staff"]
     fieldsets = [
         (None, {"fields": ["username", "password"]}),
@@ -20,9 +20,15 @@ class UserAdmin(UserAdmin):
     filter_horizontal = []
 
 
+class ExpressionAdmin(admin.ModelAdmin):
+	list_display = ["id", "content", "translation_it", "level", "is_phrasal_verb"]
+	search_fields = ["content", "translation_it"]
+	ordering = ["content", "translation_it", "level"]
+
+
 admin.site.register(Role)
 admin.site.register(Level)
-admin.site.register(Expression)
+admin.site.register(Expression, ExpressionAdmin)
 admin.site.register(User, UserAdmin)
 
 admin.site.unregister(Group)
