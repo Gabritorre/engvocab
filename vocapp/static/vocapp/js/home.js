b53e299bac290579257542a56ef9f700ae765070
Gabritorre <gabritorredev@gmail.com>
2024-06-30 18:43:05 +0200

    Make UI from home page

--- vocapp/static/vocapp/js/home.js
+++ vocapp/static/vocapp/js/home.js
@@ -1,6 +1,7 @@
 function show_solution() {
 	let show_button = document.getElementById("show_button");
-	let solution_div = document.getElementById("solution");
+	let solution_div = document.getElementById("hidden_solution");
 	show_button.hidden = true;
 	solution_div.removeAttribute("hidden")
+	solution_div.classList.add("solution");
 }
