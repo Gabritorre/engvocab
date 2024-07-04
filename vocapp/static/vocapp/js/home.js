9e73178a396ae6cbbd3fd159058a5e7be3a9a964
Gabritorre <gabritorredev@gmail.com>
2024-07-04 17:09:46 +0200

    Add style to filters buttons

--- vocapp/static/vocapp/js/home.js
+++ vocapp/static/vocapp/js/home.js
@@ -5,3 +5,17 @@ function show_solution() {
 	solution_div.removeAttribute("hidden")
 	solution_div.classList.add("solution");
 }
+
+function filters_button_style() {
+	let filters = document.getElementsByClassName("filter");
+	for (let i = 0; i < filters.length; i++) {
+		filters[i].children[0].addEventListener("change", function(event) {
+			if (event.currentTarget.checked) {
+				filters[i].children[1].classList.add("filter_on");
+			}
+			else {
+				filters[i].children[1].classList.remove("filter_on");
+			}
+		});
+	}
+}
